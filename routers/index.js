6ecff0cad29d7bed69ee8570d384f8b630cfc54a
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-07 13:18:06 -0400

    asdf

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/', (req,res) => {
-    // res.json({info: 'Node.js, Express, and Postgres API'})
-})
+// router.get('/', (req,res) => {
+//     // res.json({info: 'Node.js, Express, and Postgres API'})
+// })
 
 router.use(require('./user'))
 
