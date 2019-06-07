266408d22eeb0063a75681304f552a10d595a184
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-07 11:52:37 -0400

    heroku updates

--- api/userApi.js
+++ api/userApi.js
@@ -1,11 +1,11 @@
-const pool = require('../connection')
+const client = require('../connection')
 
 getUsers = () => {
-    return pool.query('SELECT * FROM users ORDER BY id ASC')
+    return client.query('SELECT * FROM users ORDER BY id ASC')
 }
 
 getUserById = (id) => {
-    return pool.query(`SELECT * FROM users WHERE id = ${id}`)
+    return client.query(`SELECT * FROM users WHERE id = ${id}`)
 }
 
 module.exports = { getUsers, getUserById }
--- connection.js
+++ connection.js
@@ -1,10 +1,21 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user: 'sam',
-    host: 'localhost',
-    database: 'flightpath',
-    password: 'barksdale',
-    post: '5432'
-}) || new Pool({connectionString: process.env.DATABASE_URL, ssl: true,});
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: 'sam',
+//     host: 'localhost',
+//     database: 'flightpath',
+//     password: 'barksdale',
+//     post: '5432'
+// }) || new Pool({connectionString: process.env.DATABASE_URL, ssl: true,});
 
-module.exports = pool;
+// module.exports = pool;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+module.exports = client;
