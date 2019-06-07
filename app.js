1b997c79f49b0c01f7dd17fde33b2ae907455122
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-07 11:39:52 -0400

    updated connection

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(
-    bodyParser.urlencoded({ extended: true })
-)
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./routers'))
 
 const PORT = process.env.PORT || 8000
