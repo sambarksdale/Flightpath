a30c6d448be474057f06ab0a07ec9717bee939a0
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-07 12:54:16 -0400

    created react app

--- connection.js
+++ connection.js
@@ -1,20 +1,19 @@
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//     user: 'sam',
-//     host: 'localhost',
-//     database: 'flightpath',
-//     password: 'barksdale',
-//     post: '5432'
-// }) || new Pool({connectionString: process.env.DATABASE_URL, ssl: true,});
+const Pool = require('pg').Pool
+const client = new Pool({
+    user: 'sam',
+    host: 'localhost',
+    database: 'flightpath',
+    password: 'barksdale',
+    post: '5432'
+}) || new Pool({connectionString: process.env.DATABASE_URL, ssl: true,});
 
-// module.exports = pool;
+// const { Client } = require('pg');
 
-const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
 
 client.connect();
 
