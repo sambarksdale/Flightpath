26d316cbd8e95c90828ad5d410b8d4061b173dce
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-07 13:13:55 -0400

    updated heroku connection

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./routers'))
+app.use(express.static(__dirname + '/client/build/'));
+
+app.get('/', (req,res) => {
+res.sendFile(__dirname + '/client/build/index.html')
+})
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
