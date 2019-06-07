ee7e97f19a110195df49ccfc7b393bfb47bee70b
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-07 12:56:44 -0400

    updated client app

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req,res) => {
-    res.json({info: 'Node.js, Express, and Postgres API'})
+    // res.json({info: 'Node.js, Express, and Postgres API'})
 })
 
 router.use(require('./user'))
