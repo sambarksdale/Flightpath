5c40f85e1e68c75e5e5ffa572faef75a3752a9a0
Samuel Barksdale <samuelbarksdale@Samuels-MacBook-Air.local>
2019-06-10 09:21:39 -0400

    created heroku dev branch

--- connection.js
+++ connection.js
@@ -1,19 +1,19 @@
-const Pool = require('pg').Pool
-const client = new Pool({
-    user: 'sam',
-    host: 'localhost',
-    database: 'flightpath',
-    password: 'barksdale',
-    post: '5432'
-}) || new Pool({connectionString: process.env.DATABASE_URL, ssl: true,});
+// const Pool = require('pg').Pool
+// const client = new Pool({
+//     user: 'sam',
+//     host: 'localhost',
+//     database: 'flightpath',
+//     password: 'barksdale',
+//     post: '5432'
+// }) || new Pool({connectionString: process.env.DATABASE_URL, ssl: true,});
 
-// const { Client } = require('pg');
+const { Client } = require('pg');
 
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
 
 client.connect();
 
